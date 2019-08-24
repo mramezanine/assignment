df13344d486df165a19c7ff8f7e6419e3755d0f7
Mahdi Ramezani <mramezanine@gmail.com>
2019-08-24 09:01:05 +0430

    Big Bang (Start of project)

---
+++ src/main/java/com/mahdi/assignment/paymentplan/PaymentPlanApplication.java
@@ -0,0 +1,13 @@
+package com.mahdi.assignment.paymentplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentPlanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentPlanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mahdi/assignment/paymentplan/PaymentPlanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mahdi.assignment.paymentplan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaymentPlanApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
