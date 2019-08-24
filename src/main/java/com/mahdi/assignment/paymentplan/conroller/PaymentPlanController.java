f57ae8673fdd61d6a3fc0ddb98e633fc56416361
Mahdi Ramezani <mramezanine@gmail.com>
2019-08-24 09:43:09 +0430

    add service with dummy data

--- src/main/java/com/mahdi/assignment/paymentplan/conroller/PaymentPlanController.java
+++ src/main/java/com/mahdi/assignment/paymentplan/conroller/PaymentPlanController.java
@@ -3,6 +3,8 @@ package com.mahdi.assignment.paymentplan.conroller;
 
 import com.mahdi.assignment.paymentplan.model.Annuity;
 import com.mahdi.assignment.paymentplan.model.LoanCondition;
+import com.mahdi.assignment.paymentplan.service.PaymentPlanService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -13,23 +15,14 @@ import java.util.List;
 @RequestMapping("/generate-plan")
 public class PaymentPlanController {
 
+
+    @Autowired
+    PaymentPlanService paymentPlanService;
+
     @PostMapping
     public List<Annuity> calculatePlan(@RequestBody LoanCondition loanCondition){
-        List<Annuity> result = Arrays.asList(
-                                    new Annuity("5000.0",
-                                            new Date(),
-                                            "5.0",
-                                            "10.0",
-                                            "200.0",
-                                            "10.0") ,
-                                     new Annuity("1000.0",
-                                            new Date(),
-                                            "10.0",
-                                            "2.0",
-                                            "20.0",
-                                            "20.0")
-                                );
-        return result;
+
+        return paymentPlanService.calculatePlan(loanCondition);
     }
 
 
