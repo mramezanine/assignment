7dafd9f74db8bf44701df5584f4dcea2e3def688
Mahdi Ramezani <mramezanine@gmail.com>
2019-08-25 11:57:24 +0430

    add data to swagger

--- src/main/java/com/mahdi/assignment/paymentplan/config/SwaggerConfig.java
+++ src/main/java/com/mahdi/assignment/paymentplan/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.mahdi.assignment.paymentplan.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,6 +19,13 @@ public class SwaggerConfig {
                 .select()
             .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.any())
-            .build();
+            .build().apiInfo(apiEndPointsInfo());
     }
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Generate plan for loan repayment - REST API")
+                .description("Generate plan REST API")
+                .version("1.0.0")
+                .build();
+    }
+
 }
--- src/main/java/com/mahdi/assignment/paymentplan/conroller/PaymentPlanController.java
+++ src/main/java/com/mahdi/assignment/paymentplan/conroller/PaymentPlanController.java
@@ -4,10 +4,14 @@ package com.mahdi.assignment.paymentplan.conroller;
 import com.mahdi.assignment.paymentplan.model.Annuity;
 import com.mahdi.assignment.paymentplan.model.LoanCondition;
 import com.mahdi.assignment.paymentplan.service.PaymentPlanService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/generate-plan")
@@ -17,6 +21,20 @@ public class PaymentPlanController {
     @Autowired
     PaymentPlanService paymentPlanService;
 
+    @ApiOperation(value = "Calculate a plan for loan repayment, based on 'Amount(float type)'," +
+            " 'nominal rate(float type)', 'duration(integer type)' and 'start date(string type, like 2019-09-01T00:00:01Z)'. " +
+            "For more information please check: https://financeformulas.net/Annuity_Payment_Formula.html ",
+            response = Annuity.class,
+            responseContainer = "List")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            @ApiResponse(code = 422, message = "Input data is not valid. Please check Duration/NominalRate"),
+            @ApiResponse(code = 500, message = "Internal error")
+    })
     @PostMapping
     public List<Annuity> calculatePlan(@RequestBody LoanCondition loanCondition) throws Exception {
         return paymentPlanService.calculatePlan(loanCondition);
